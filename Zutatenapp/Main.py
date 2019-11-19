df4966743bfc2ee1e13800e9001ee7b10dc81657
Nogythe <47657453+Nogythe@users.noreply.github.com>
2019-11-19 12:54:22 +0100

    safe and load json

--- Zutatenapp/Main.py
+++ Zutatenapp/Main.py
@@ -1,3 +1,5 @@
+import os
+from pathlib import Path
 from flask import Flask
 from flask import render_template
 from libs import data
@@ -6,8 +8,13 @@ from flask import redirect
 from flask import url_for
 
 app = Flask("Zutatenapp")
+app_main_path = Path(os.path.abspath("/".join(os.path.realpath(__file__).split("/")[:-1])))
+data_path = Path(os.path.abspath(app_main_path / "data"))
+data_storage_file = data_path / "rezepte.json"
+
 rezepte = {}
-rezepte = data.create_dummy_data()
+#rezepte = data.create_dummy_data()
+rezepte = data.load_json(data_storage_file)
 
 @app.route("/")
 def home():
@@ -22,8 +29,12 @@ def add_franz():
     if request.method == 'POST':
         rezept_titel = request.form['rezept_titel']
         rezept_beschreibung = request.form['rezept_beschreibung']
+        if not 'franz' in rezepte:
+            rezepte['franz'] = {}
         rezepte['franz'][rezept_titel] = rezept_beschreibung
+        data.save_json(data_storage_file, rezepte)
         return redirect(url_for('summary_franz'))
+
     return render_template('add_franz.html')
 
 
@@ -32,7 +43,10 @@ def add_italy():
     if request.method == 'POST':
         rezept_titel = request.form['rezept_titel']
         rezept_beschreibung = request.form['rezept_beschreibung']
+        if not 'italy' in rezepte:
+            rezepte['italy'] = {}
         rezepte['italy'][rezept_titel] = rezept_beschreibung
+        data.save_json(data_storage_file, rezepte)
         return redirect(url_for('summary_italy'))
 
     return render_template('add_italy.html')
@@ -43,7 +57,10 @@ def add_turkey():
     if request.method == 'POST':
         rezept_titel = request.form['rezept_titel']
         rezept_beschreibung = request.form['rezept_beschreibung']
+        if not 'turkey' in rezepte:
+            rezepte['turkey'] = {}
         rezepte['turkey'][rezept_titel] = rezept_beschreibung
+        data.save_json(data_storage_file, rezepte)
         return redirect(url_for('summary_turkey'))
 
     return render_template('add_turkey.html', methods=['GET', 'POST'])
@@ -62,7 +79,10 @@ def summary_franz():
 
 @app.route('/summary_italy')
 def summary_italy():
-    return render_template('summary_italy.html', rezepte=rezepte['italy'])
+    if 'italy' in rezepte:
+        return render_template('summary_italy.html', rezepte=rezepte['italy'])
+    else: 
+        return render_template('summary_italy.html', rezepte=None)
 
 
 @app.route('/summary_turkey')
--- Zutatenapp/libs/data.py
+++ Zutatenapp/libs/data.py
@@ -1,3 +1,22 @@
+import json
+
+
+def load_json(json_path):
+    try:
+        with open(json_path) as open_file:
+            rezepte = json.load(open_file)
+    except FileNotFoundError:
+        rezepte = {}
+
+    return rezepte
+
+
+def save_json(json_path, data):
+    with open(json_path, "w+", encoding="utf-8") as open_file:
+        json.dump(data, open_file, indent=4)
+
+
+
 def create_dummy_data():
     rezepte = {
         'italy': {
