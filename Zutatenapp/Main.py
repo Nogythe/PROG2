073815768da00f74a4406af297e27e635b6d5bf2
Nogythe <47657453+Nogythe@users.noreply.github.com>
2019-11-19 09:51:13 +0100

    Restrukturierung des Ordners

--- Zutatenapp/Main.py
+++ Zutatenapp/Main.py
@@ -3,94 +3,26 @@ from flask import render_template
 
 app = Flask("Zutatenapp")
 
-@app.route('/')
+
+@app.route("/")
 def home():
-	return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/Franz')
-def Franz():
-	return render_template('franz.html')
 
-@app.route('/Italy')
-def Italy():
-	return render_template('franz.html')
+@app.route('/franz')
+def franz():
+    return render_template('franz.html')
 
-@app.route('/Turkey')
-def Turkey():
-	return render_template('franz.html')
 
-if __name__ == "_main_":
-	app.run(debug=True, port=5000)
+@app.route('/italy')
+def italy():
+    return render_template('italy.html')
 
-	"""Neu"""
 
-	menu = [
-        ('Neues Rezept', add),
-        ('Zeige Rezepte', show)
-    ]
+@app.route('/turkey')
+def turkey():
+    return render_template('turkey.html')
 
-    from collections import defaultdict as DefaultDict
-import json
 
-def load():
-    try:
-        with open('recipes.txt') as file_:
-            recipes = DefaultDict(list, json.load(file_))
-    except IOError:
-        recipes = DefaultDict(list)
-    return recipes
-
-def save(recipes):
-    with open('recipes.txt', 'w') as file_:
-        json.dump(recipes, file_)
-
-def get_category(prompt, recipes):
-    categories = recipes.keys()
-    for number, name in enumerate(categories):
-        print '{:2d}: {}'.format(number, name)
-    category = raw_input(prompt)
-    try:
-        category = categories[int(category)]
-    except (ValueError, IndexError):
-        pass
-    return category
-
-def add(recipes):
-    try:
-        recipe = raw_input('Ihr Rezepttext (Ctrl-C beendet): ')
-    except KeyboardInterrupt:
-        return
-    category = get_category(
-        'Unter welche Kategorie faellt Rezept? ', recipes
-    )
-    recipes[category].append(recipe)
-
-def show(recipes):
-    if recipes:
-        category = get_category(
-            'Aus welcher Kategorie sollen Rezepte gezeigt werden? ', recipes
-        )
-        for recipe in recipes[category]:
-            print '\n{}\n***\n'.format(recipe)
-    else:
-        print 'Keine Rezepte vorhanden!\n'
-
-def run():
-    menu = [
-        ('Neues Rezept', add),
-        ('Zeige Rezepte', show)
-    ]
-    recipes = load()
-    while True:
-        print
-        for item_number, item in enumerate(menu):
-            print '{:2d}: {}'.format(item_number, item[0])
-        try:
-            choice = int(raw_input('Ihre Wahl (Ctrl-C beendet): '))
-        except (ValueError, IndexError):
-            print 'Bitte gueltige Nummer eingeben...',
-        except KeyboardInterrupt:
-            break
-        print
-        menu[choice][1](recipes)
-    save(recipes)
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
